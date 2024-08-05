7a1993e705fb30152c985d4e71820a41fe4f9da7
Reuben Tier <otterlord.dev@gmail.com>
2024-08-05 12:09:30 +0100

    Fix feedback button

--- packages/opinion-otter/src/index.ts
+++ packages/opinion-otter/src/index.ts
@@ -1,16 +1,13 @@
 import type { StarlightPlugin } from '@astrojs/starlight/types'
-import icon from 'astro-icon'
 
 export default function integration(): StarlightPlugin {
   return {
     name: 'opinion-otter/starlight',
     hooks: {
       setup: ({ addIntegration, astroConfig, updateConfig }) => {
-        if (!astroConfig.integrations.find(i => i.name === 'astro-icon')) addIntegration(icon({ include: { 'material-symbols': ['*'] } }))
-
         updateConfig({
           components: {
-            PageSidebar: 'opinion-otter/components/PageSidebar.astro'
+            Pagination: 'opinion-otter/components/Pagination.astro'
           }
         })
       }
